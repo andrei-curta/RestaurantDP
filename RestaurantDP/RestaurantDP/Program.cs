4ff9a63b797389f9c32495cddcfc6e3f28534ae5
Larisa Cimpoesu <larissa9850@gmail.com>
2020-03-31 13:13:42 +0300

    Incercare commit

--- RestaurantDP/RestaurantDP/Program.cs
+++ RestaurantDP/RestaurantDP/Program.cs
@@ -6,7 +6,7 @@ namespace RestaurantDP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); //
         }
     }
 }
